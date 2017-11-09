8b19b35a3158424538537b31357019f1a57bee5e
developer <developer@mail.com>
2017-11-09 15:53:43 +0200

    change design alert dialog

--- app/src/main/java/com/example/developer/themontihallparadox/MainActivity.kt
+++ app/src/main/java/com/example/developer/themontihallparadox/MainActivity.kt
@@ -1,10 +1,11 @@
 package com.example.developer.themontihallparadox
 
 import android.animation.ValueAnimator
+import android.app.Dialog
 import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.support.v4.content.ContextCompat
-import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.view.Window
@@ -21,7 +22,16 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var newGameButton: Button
     private lateinit var actionsTextView: TextView
 
+    private var buttonOk: Button? = null
+    private var buttonNo: Button? = null
+    private var textViewTitle: TextView? = null
+    private var textViewSubTitle: TextView? = null
+
+    private lateinit var dialog: Dialog
+
     private var chosenDoor: Door? = null
+    private var closedDoor: Door = Door.ONE
+
     private var winDoor = Door.ONE
 
     private var random = Random()
@@ -37,6 +47,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         window.setFlags(LayoutParams.FLAG_FULLSCREEN, LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_main)
 
+        createDialog()
         actionsTextView = findViewById(R.id.textView_actions)
 
         doorImageViews = Door.values().map {
@@ -49,9 +60,31 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         startNewGame()
     }
 
+    private fun createDialog() {
+        dialog = Dialog(this@MainActivity)
+        val view = layoutInflater.inflate(R.layout.alert_dialog, null)
+        view.apply {
+            textViewTitle = findViewById(R.id.textView_alert_title)
+            textViewSubTitle = findViewById(R.id.textView_alert_subtitle)
+            buttonOk = findViewById(R.id.button_yes)
+            buttonOk?.setOnClickListener {
+                showResults(closedDoor)
+                dialog.dismiss()
+            }
+            buttonNo = findViewById(R.id.button_no)
+            buttonNo?.setOnClickListener {
+                chosenDoor?.let { it1 -> showResults(it1) }
+                dialog.dismiss()
+            }
+        }
+        dialog.window.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        dialog.setOnDismissListener { newGameButton.visibility = View.VISIBLE }
+        dialog.setContentView(view)
+    }
+
+
     private fun startNewGame() {
         winDoor = Door.values()[random.nextInt(Door.values().size)]
-
         chosenDoor = null
 
         valueAnimator.cancel()
@@ -105,20 +138,14 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     private fun askToChangeChoice(chosenDoor: Door, openedDoor: Door) {
-        val closedDoor = Door.values().first { it != chosenDoor && it != openedDoor }
+        closedDoor = Door.values().first { it != chosenDoor && it != openedDoor }
 
-        AlertDialog.Builder(this@MainActivity, R.style.AppTheme_AlertDialog)
-                .setTitle(getString(R.string.action_door_number_is_open, getString(openedDoor.getTitleResId())))
-                .setMessage(getString(R.string.question_change_your_choice, getString(chosenDoor.getTitleResId()), getString(closedDoor.getTitleResId())))
-                .setPositiveButton(R.string.yes, { _, _ ->
-                    showResults(closedDoor)
-                })
-                .setNegativeButton(R.string.no, { _, _ ->
-                    showResults(chosenDoor)
-                })
-                .setCancelable(false)
-                .setOnDismissListener { newGameButton.visibility = View.VISIBLE }
-                .show()
+        textViewTitle?.text = getString(R.string.action_door_number_is_open, getString(openedDoor.getTitleResId()))
+        textViewSubTitle?.text = getString(R.string.question_change_your_choice,
+                getString(chosenDoor.getTitleResId()),
+                getString(closedDoor.getTitleResId()))
+
+        dialog.show()
     }
 
     private fun showResults(chosenDoor: Door) {
